91a25ed3bb3727df152587018a8102e68c05fcb1
minaripenguin <minaripenguin@users.noreply.github.com>
2023-09-17 00:09:38 +0800

    AfterHome: Launch apps in freeform mode system shortcut
    Change-Id: I3b68d38509cbe45890bfab49435be2e08ea04e26
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- src/com/android/launcher3/popup/SystemShortcut.java
+++ src/com/android/launcher3/popup/SystemShortcut.java
@@ -2,6 +2,7 @@ package com.android.launcher3.popup;
 
 import static com.android.launcher3.logging.StatsLogManager.LauncherEvent.LAUNCHER_PRIVATE_SPACE_INSTALL_SYSTEM_SHORTCUT_TAP;
 import static com.android.launcher3.logging.StatsLogManager.LauncherEvent.LAUNCHER_PRIVATE_SPACE_UNINSTALL_SYSTEM_SHORTCUT_TAP;
+import static android.app.WindowConfiguration.WINDOWING_MODE_FREEFORM;
 import static android.content.pm.SuspendDialogInfo.BUTTON_ACTION_UNSUSPEND;
 
 import static com.android.launcher3.logging.StatsLogManager.LauncherEvent.LAUNCHER_SYSTEM_SHORTCUT_APP_INFO_TAP;
@@ -10,6 +11,8 @@ import static com.android.launcher3.logging.StatsLogManager.LauncherEvent.LAUNCH
 import static com.android.launcher3.LauncherSettings.Favorites.ITEM_TYPE_APPLICATION;
 import static com.android.launcher3.LauncherSettings.Favorites.ITEM_TYPE_TASK;
 
+import android.app.Activity;
+import android.app.ActivityManagerNative;
 import android.app.ActivityOptions;
 import android.content.ComponentName;
 import android.app.AlertDialog;
@@ -29,6 +32,7 @@ import android.net.Uri;
 import android.os.RemoteException;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowInsets;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -507,6 +511,41 @@ public abstract class SystemShortcut<T extends ActivityContext> extends ItemInfo
             } catch (URISyntaxException e) {
                 // Do nothing.
             }
+        }
+    }
+
+    public static final Factory<BaseDraggingActivity> FREE_FORM = (activity, itemInfo, originalView) -> 
+        new FreeForm(activity, itemInfo, originalView);
+
+    public static class FreeForm extends SystemShortcut<BaseDraggingActivity> {
+        private final String mPackageName;
+
+        public FreeForm(BaseDraggingActivity target, ItemInfo itemInfo, View originalView) {
+            super(R.drawable.ic_caption_desktop_button_foreground, R.string.recent_task_option_freeform, target, itemInfo, originalView);
+            mPackageName = itemInfo.getTargetComponent().getPackageName();
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mPackageName != null) {
+                Intent intent = mTarget.getPackageManager().getLaunchIntentForPackage(mPackageName);
+                if (intent != null) {
+                    ActivityOptions options = makeLaunchOptions(mTarget);
+                    mTarget.startActivity(intent, options.toBundle());
+                    AbstractFloatingView.closeAllOpenViews(mTarget);
+                }
+            }
+        }
+
+        private ActivityOptions makeLaunchOptions(Activity activity) {
+            ActivityOptions activityOptions = ActivityOptions.makeBasic();
+            activityOptions.setLaunchWindowingMode(WINDOWING_MODE_FREEFORM);
+            final View decorView = activity.getWindow().getDecorView();
+            final WindowInsets insets = decorView.getRootWindowInsets();
+            final Rect r = new Rect(0, 0, decorView.getWidth() / 2, decorView.getHeight() / 2);
+            r.offsetTo(insets.getSystemWindowInsetLeft() + 50, insets.getSystemWindowInsetTop() + 50);
+            activityOptions.setLaunchBounds(r);
+            return activityOptions;
         }
     }
 
