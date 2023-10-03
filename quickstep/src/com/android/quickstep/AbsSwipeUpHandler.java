e0d95d89549b539daf63197653a961b3c724bea0
nift4 <nift4@protonmail.com>
2023-10-03 23:30:41 +0200

    AfterHome: Fix overview scrolling scale animation
    Adapt it for Live Tile support + fix landscape handling
    
    Change-Id: I1cf5d79aa6373e836d33bc7ddb15c0c5258a63d7
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
+++ quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
@@ -2534,6 +2534,7 @@ public abstract class AbsSwipeUpHandler<T extends StatefulActivity<S>,
             startInterceptingTouchesForGesture();
         }
         for (RemoteTargetHandle remoteHandle : mRemoteTargetHandles) {
+            float scrollScale = setRecentsScroll ? mRecentsView.getScrollScale(remoteHandle) : 1f;
             AnimatorControllerWithResistance playbackController =
                     remoteHandle.getPlaybackController();
             if (playbackController != null) {
@@ -2542,6 +2543,7 @@ public abstract class AbsSwipeUpHandler<T extends StatefulActivity<S>,
 
             if (notSwipingToHome) {
                 TaskViewSimulator taskViewSimulator = remoteHandle.getTaskViewSimulator();
+                taskViewSimulator.scrollScale.value = scrollScale;
                 if (setRecentsScroll) {
                     taskViewSimulator.setScroll(scrollOffset);
                 }
