201d75e13a1a844ebcc575a4dde510d86df5eb95
Dhina17 <dhinalogu@gmail.com>
2024-06-19 14:05:21 +0530

    fixup! AfterHome: fix overview scrolling scale animation
    Change-Id: I181328b363d2f6d0c3a772384fd6d1d3be979ceb

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -3687,7 +3687,7 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
                                 if (match) {
                                     anim.addOnFrameCallback(() -> {
                                         rth.getTaskViewSimulator().scrollScale.value =
-                                                mOrientationHandler.getPrimaryValue(
+                                                getPagedOrientationHandler().getPrimaryValue(
                                                     tv.getScaleX(),
                                                     tv.getScaleY()
                                                 );
@@ -6211,7 +6211,7 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
                 for (int id : tv.getTaskIds()) {
                     if (targets != null && targets.findTask(id) != null) {
                         rth.getTaskViewSimulator().scrollScale.value =
-                                mOrientationHandler.getPrimaryValue(
+                                getPagedOrientationHandler().getPrimaryValue(
                                     tv.getScaleX(),
                                     tv.getScaleY()
                                 );
@@ -6231,7 +6231,7 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
             RemoteAnimationTargets targets = params.getTargetSet();
             for (int id : tv.getTaskIds()) {
                 if (targets != null && targets.findTask(id) != null) {
-                    return mOrientationHandler.getPrimaryValue(
+                    return getPagedOrientationHandler().getPrimaryValue(
                                 tv.getScaleX(),
                                 tv.getScaleY()
                            );
