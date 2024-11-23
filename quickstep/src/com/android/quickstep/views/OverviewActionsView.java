e476a0526712d7a3eccf3f56af35832d7689260d
anulalat <ndra.bodoh@gmail.com>
2024-11-23 12:24:30 +0100

    Revert "Launcher3: Allow locking tasks to recents [2/2]"
    This reverts commit 5a0b8e4a7ddded0176763264256aec63e24d5c21.

--- quickstep/src/com/android/quickstep/views/OverviewActionsView.java
+++ quickstep/src/com/android/quickstep/views/OverviewActionsView.java
@@ -105,7 +105,6 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
     private static final String KEY_RECENTS_SCREENSHOT = "pref_recents_screenshot";
     private static final String KEY_RECENTS_CLEAR_ALL = "pref_recents_clear_all";
     private static final String KEY_RECENTS_LENS = "pref_recents_lens";
-    private static final String KEY_RECENTS_LOCK = "pref_recents_lock";
 
     private MultiValueAlpha mMultiValueAlpha;
     private Button mSplitButton;
@@ -132,7 +131,6 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
     private boolean mScreenshot;
     private boolean mClearAll;
     private boolean mLens;
-    private boolean mLock;
 
     public OverviewActionsView(Context context) {
         this(context, null);
@@ -148,7 +146,6 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
         mScreenshot = prefs.getBoolean(KEY_RECENTS_SCREENSHOT, true);
         mClearAll = prefs.getBoolean(KEY_RECENTS_CLEAR_ALL, true);
         mLens = prefs.getBoolean(KEY_RECENTS_LENS, false);
-        mLock = prefs.getBoolean(KEY_RECENTS_LOCK, true);
         prefs.registerOnSharedPreferenceChangeListener(this);
     }
 
@@ -175,10 +172,6 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
         lens.setOnClickListener(this);
         lens.setVisibility(mLens && Utilities.isGSAEnabled(getContext()) ? VISIBLE : GONE);
         findViewById(R.id.lens_space).setVisibility(mLens && Utilities.isGSAEnabled(getContext()) ? VISIBLE : GONE);
-        View actionLock = findViewById(R.id.action_lock);
-        actionLock.setOnClickListener(this);
-        actionLock.setVisibility(mLock ? VISIBLE : GONE);
-        findViewById(R.id.action_lock_space).setVisibility(mLock ? VISIBLE : GONE);
 
         mSplitButton = findViewById(R.id.action_split);
         mSplitButton.setOnClickListener(this);
@@ -235,10 +228,7 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
             mClearAll = prefs.getBoolean(KEY_RECENTS_CLEAR_ALL, true);
         } else if (key.equals(KEY_RECENTS_LENS)) {
             mLens = prefs.getBoolean(KEY_RECENTS_LENS, false);
-        } else if (key.equals(KEY_RECENTS_LOCK)) {
-            mLock = prefs.getBoolean(KEY_RECENTS_LOCK, false);
         }
-        
         updateVisibilities();
     }
 
